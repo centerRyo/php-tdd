f90b942f000e58263e21ab4bbf1fe0a473285a42
Ryo Nakadoi <r_nakadoi@corp.allabout.co.jp>
2019-07-05 19:03:02 +0900

    引数でとった値を文字列で返す本実装をした

--- tests/FizzBuzzTest.php
+++ tests/FizzBuzzTest.php
@@ -5,7 +5,6 @@ namespace Test;
 use Tests\TestCase;
 use App\FizzBuzz;
 
-// 引数にとった値を文字列で返す
 // ただし3の倍数の時はFizzを返す
 // ただし5の倍数の時はBuzzを返す
 // ただし3と5の両方の倍数の場合はFizzBuzzを返す
@@ -16,6 +15,6 @@ class FizzBuzzTest extends TestCase
     /** @test */
     public function 引数にとった値を文字列で返す()
     {
-    $this->assertEquals('中土井', FizzBuzz::FizzBuzz('なかどい'));
+    $this->assertEquals('なかどい', FizzBuzz::FizzBuzz('なかどい'));
     }
 }
