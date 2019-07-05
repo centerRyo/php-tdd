403a92b3c482dce09c0d99b12e0de1de56f8128e
Ryo Nakadoi <r_nakadoi@corp.allabout.co.jp>
2019-07-05 18:45:06 +0900

    実装するプログラムの要件を記述

--- tests/FizzBuzzTest.php
+++ tests/FizzBuzzTest.php
@@ -4,6 +4,12 @@ namespace Test;
 
 use Tests\TestCase;
 
+// 引数にとった値を文字列で返す
+// ただし3の倍数の時はFizzを返す
+// ただし5の倍数の時はBuzzを返す
+// ただし3と5の両方の倍数の場合はFizzBuzzを返す
+// 引数が１から100までの数でない場合エラー
+
 class FizzBuzzTest extends TestCase
 {
     public function testhoge()
