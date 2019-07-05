4729b6bd5c7398672cc5ceea9207da46eafd099f
Ryo Nakadoi <r_nakadoi@corp.allabout.co.jp>
2019-07-05 19:28:33 +0900

    受け取った引数を文字列で返す関数のリファクタリングをした

--- src/FizzBuzz.php
+++ src/FizzBuzz.php
@@ -6,18 +6,6 @@ class FizzBuzz
 {
     public static function FizzBuzz($name)
     {
-        // 数値以外の値だったらそのまま返す、数値だったら数値文字列にして返す
-        if (ctype_digit("$name")) {
-            return "$name";
-        } else {
-            return $name;
-        }
-        // if ($name === 'なかどい') {
-        //     return 'なかどい';
-        // } elseif ($name === 1) {
-        //     return '1';
-        // } else {
-        //     return '2';
-        // }
+        return "$name";
     }
 }
