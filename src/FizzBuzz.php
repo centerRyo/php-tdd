b256ff6bb7eec042f28d3b43dbdbcfdc9ca89dfc
Ryo Nakadoi <r_nakadoi@corp.allabout.co.jp>
2019-07-05 19:24:59 +0900

    引数にとった値を文字列で返す本実装
    数値も数値の文字列で返す

--- src/FizzBuzz.php
+++ src/FizzBuzz.php
@@ -6,12 +6,18 @@ class FizzBuzz
 {
     public static function FizzBuzz($name)
     {
-        if ($name === 'なかどい') {
-            return 'なかどい';
-        } elseif ($name === 1) {
-            return '1';
+        // 数値以外の値だったらそのまま返す、数値だったら数値文字列にして返す
+        if (ctype_digit("$name")) {
+            return "$name";
         } else {
-            return '2';
+            return $name;
         }
+        // if ($name === 'なかどい') {
+        //     return 'なかどい';
+        // } elseif ($name === 1) {
+        //     return '1';
+        // } else {
+        //     return '2';
+        // }
     }
 }
