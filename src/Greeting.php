71699038b600b93e4af620199b31d72fc9fcd915
Ryo Nakadoi <r_nakadoi@corp.allabout.co.jp>
2019-07-22 19:32:57 +0900

    PM5時からAM0時の場合はこんばんはと返す、AM5時からAM11時の場合はおはようございますと返す

--- src/Greeting.php
+++ src/Greeting.php
@@ -4,8 +4,27 @@ namespace App;
 
 class Greeting
 {
+    private $time;
+
+    public function __construct($time = 0)
+    {
+        $this->time = $time - 1;
+    }
+
     public function call()
     {
-        return 'おはようございます';
+        if ($this->time >= 5 && $this->time <= 11) {
+            return 'おはようございます';
+        }
+        // 1.PM5時からAM0時の場合はこんばんはと返す
+        // 2.AM5時からAM11時の場合はおはようございますと返す
+        // 3.AM11時からPM5時の場合はこんにちはと返す
+        // 4.AM0時からAM5時の間は寝ているので何も返さない
+        // if (date('H') >= '17') {
+        //     return 'こんばんは';
+        // }
+        if ($this->time >= 17) {
+            return 'こんばんは';
+        }
     }
 }
--- tests/GreetingTest.php
+++ tests/GreetingTest.php
@@ -6,16 +6,32 @@ use App\Greeting;
 
 class GreetingTest extends TestCase
 {
+    public function setUp(): void
+    {
+        parent::setUp();
+        date_default_timezone_set('Asia/Tokyo');
+    }
 
-    // AM5時からAM11時の場合はおはようございますと返す
-    // AM11時からPM5時の場合はこんにちはと返す
-    // PM5時からAM0時の場合はこんばんはと返す
-    // AM0時からAM5時の間は寝ているので何も返さない
+    // 1.PM5時からAM0時の場合はこんばんはと返す
+    // 2.AM5時からAM11時の場合はおはようございますと返す
+    // 3.AM11時からPM5時の場合はこんにちはと返す
+    // 4.AM0時からAM5時の間は寝ているので何も返さない
+
+    /** @test */
+    public function PM5時からAM0時の場合はこんばんはと返す()
+    {
+        $time = 19;
+        $greeting = new Greeting($time);
+        var_dump($greeting);
+        $this->assertEquals('こんばんは', $greeting->call());
+    }
 
     /** @test */
     public function AM5時からAM11時の場合はおはようございますと返す()
     {
-        $greeting = new Greeting();
+        $time = date('H', mktime(10));
+        $greeting = new Greeting($time);
+        var_dump($greeting);
         $this->assertEquals('おはようございます', $greeting->call());
     }
 }
