0d2c218ccf38a2cec24458c265e0b0c84ac629ff
Ryo Nakadoi <r_nakadoi@corp.allabout.co.jp>
2019-08-02 19:22:11 +0900

    閉店時間中を表現するオブジェクトの実装

---
+++ src/ClosedShop.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+class ClosedShop
+{
+    public function greet()
+    {
+        return '営業時間は5時から24時までです';
+    }
+}
---
+++ tests/ClosedShopTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests;
+
+use Tests\TestCase;
+use App\ClosedShop;
+
+class ClosedShopTest extends TestCase
+{
+    /** @test */
+    public function 挨拶したら営業時間を返す()
+    {
+        $closed_shop = new ClosedShop();
+        $this->assertEquals('営業時間は5時から24時までです', $closed_shop->greet());
+    }
+}
